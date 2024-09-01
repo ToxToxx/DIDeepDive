61bf2c57203020b3a989e8d992ff8f84c3a17648
Matvey <sinema3d00@gmail.com>
2024-09-01 11:42:26 +0300

    Initial commit

---
+++ DIDeepDive/Controllers/NumbersController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DIDeepDive.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class NumbersController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetNumber()
+        {
+            return Ok(5);
+        }
+    }
+}
