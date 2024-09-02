4961b177fd44edad55969bdb7860e453deabd2fa
Matvey <sinema3d00@gmail.com>
2024-09-02 08:41:42 +0300

    edit service

--- DIDeepDive/Services/NumbersService.cs
+++ DIDeepDive/Services/NumbersService.cs
@@ -1,11 +1,13 @@
-﻿namespace DIDeepDive.Services
+﻿using DIDeepDive.Clients;
+
+namespace DIDeepDive.Services
 {
-    public class NumbersService
+    public class NumbersService(NumbersClient _numbersClient)
     {
         private readonly int _number = new Random().Next(0, 100);
         public int GetNumber()
         {
-            return _number;
+            return _numbersClient.GetNumber();
         }
     }
 }
