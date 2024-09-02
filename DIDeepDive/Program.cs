ab9c8e6c351bdea93df5da9062c87c05d6fb90b7
Matvey <sinema3d00@gmail.com>
2024-09-02 10:10:39 +0300

    raw type of adding

--- DIDeepDive/Program.cs
+++ DIDeepDive/Program.cs
@@ -6,6 +6,14 @@ var builder = WebApplication.CreateBuilder(args);
 {
     //builder.Services.AddTransient<NumbersService>();
     builder.Services.AddScoped<NumbersService>();
+
+    ServiceDescriptor serviceDescriptor = new(
+        serviceType: typeof(INumbersClient),
+        implementationType: typeof(NumbersClient),
+        lifetime: ServiceLifetime.Singleton);
+
+    builder.Services.Add(serviceDescriptor);
+
     builder.Services.AddScoped<INumbersClient, NumbersClient>();
     builder.Services.AddScoped<INumbersClient, NumberThreeClient>();
     builder.Services.AddControllers();
