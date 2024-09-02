732620bcb799b2ddd1838fdb04dfb9deccfff6c5
Matvey <sinema3d00@gmail.com>
2024-09-02 08:41:18 +0300

    adding client which will generate numbers

---
+++ DIDeepDive/Clients/NumbersClient.cs
@@ -0,0 +1,12 @@
+﻿namespace DIDeepDive.Clients
+{
+    public class NumbersClient
+    {
+        private readonly int _number = new Random().Next(0, 100);
+    
+        public int GetNumber()
+        {
+            return _number;
+        }
+    }
+}
